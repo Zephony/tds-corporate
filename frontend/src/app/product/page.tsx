127fbd7568dc465007877505d666adf5f2b56e6a
Nirenjana <unirenjana@gmail.com>
2026-01-17 17:20:13 +0530

    clean up the frontend

---
+++ frontend/src/app/product/page.tsx
@@ -0,0 +1,19 @@
+'use client'
+
+import Menubar from '@/components/menuBar'
+
+export default function ProductPage() {
+
+
+    return (
+        <div className='page-container marketing-page'>
+            <div className='left-container'>
+                <Menubar />
+            </div>
+            <div className='main-content'>
+                Product Page
+            </div>
+        </div>
+    )
+}
+
--- frontend/src/components/requestStatus.tsx
+++
@@ -1,6 +0,0 @@
-export default function RequestStatusAlert() {
-    return <div className='request-status-wrapper'>
-        <div className='status-text'></div>
-        <div className='close-icon'></div>
-    </div>
-}
